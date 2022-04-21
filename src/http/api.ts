f337ef3a60ea42f6420bd9e2db2a2ba00fc31dd4
Lucas Fernando <lucas98fernando@gmail.com>
2022-04-21 12:31:45 -0300

    Adicionando os interceptors de requests e responses na instância do axios

--- src/http/api.ts
+++ src/http/api.ts
@@ -1,5 +1,41 @@
 import axios, { AxiosInstance } from "axios";
+import storage from "@/helpers/storage";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const loginPage = "/auth/login";
 
 export const api: AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
+
+api.interceptors.request.use(
+  (config) => {
+    const token: string = storage.getLocalAccessToken() || "";
+    if (token) config.headers = { Authorization: `Bearer ${token}` };
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.log(error.response);
+    switch (error.response.status) {
+      case 400:
+        break;
+      case 401:
+        router.push(loginPage);
+        break;
+      case 403:
+        router.push(loginPage);
+        break;
+      case 404:
+        break;
+      default:
+        router.push(loginPage);
+    }
+    return Promise.reject(error);
+  }
+);
